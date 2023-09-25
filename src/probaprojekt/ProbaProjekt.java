0e8bcba5e9e0ad7c06ffddf9d6ebcb9aeda62b6d
Christuss <szendefi.krisztofer@gmail.com>
2023-09-25 14:31:07 +0200

    commit

---
+++ src/probaprojekt/ProbaProjekt.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package probaprojekt;
+
+/**
+ *
+ * @author szendefi.k.daniel
+ */
+public class ProbaProjekt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        while (en.hasMoreElements()) {
+            Object nextElement = en.nextElement();
+            
+        }
+    }
+    
+}
